74787bfac44ceaa7c98b8c51f8f01082944b8054
jeremyzyngeraneo <jzynger@aneo.fr>
2023-11-14 09:46:57 +0100

    test: import pytest and use fixtures

--- tests/armonik_cli/test_integration.py
+++ tests/armonik_cli/test_integration.py
@@ -2,39 +2,41 @@ import armonik_cli.admin as admin
 from armonik.client.sessions import ArmoniKSessions, SessionFieldFilter, TaskOptions
 from armonik.client.tasks import ArmoniKTasks, TaskFieldFilter
 from datetime import timedelta
+import pytest
 
+@pytest.fixture
+def insecure_grpc_channel():
+    return admin.create_channel({'--endpoint': 'localhost:5001', '--ca': None})
 
-grpc_channel = admin.create_channel({'--endpoint' : 'localhost:5001', '--ca' : None})
-session_client = ArmoniKSessions(grpc_channel)
-task_client = ArmoniKTasks(grpc_channel)
+@pytest.fixture
+def session_client(insecure_grpc_channel):
+    return ArmoniKSessions(insecure_grpc_channel)
 
-
-def test_is_armonik_up():
-    session_client = admin.ArmoniKSessions(grpc_channel)
+def test_is_armonik_up(session_client):
     admin.list_sessions(session_client, admin.create_session_filter(True, False,False))
 
-def test_create_session():
+def test_create_session(session_client):
     new_session = TaskOptions(max_duration=timedelta(0,3),priority=1, max_retries=10)
     assert session_client.create_session(new_session) is not None
 
-def test_list_all_sessions():
+def test_list_all_sessions(session_client):
     sessions = admin.list_sessions(session_client, admin.create_session_filter(True, False, False))
     assert sessions is not None
     assert len(sessions) > 0 
 
-def test_list_running_sessions():
+def test_list_running_sessions(session_client):
     sessions = admin.list_sessions(session_client, admin.create_session_filter(False, True, False))
     assert sessions is not None
     assert len(sessions) > 0 
 
-def test_cancel_sessions():
+def test_cancel_sessions(session_client):
     TaskOptions(max_duration=timedelta(0,3),priority=1, max_retries=10)
     sessions = admin.list_sessions(session_client, admin.create_session_filter(False, True, False))
     admin.cancel_sessions(session_client, sessions)
     update_sessions = admin.list_sessions(session_client, admin.create_session_filter(False, True, False))
     assert len(update_sessions) == 0
     
-def test_list_cancelled_sessions():
+def test_list_cancelled_sessions(session_client):
     sessions = admin.list_sessions(session_client, admin.create_session_filter(False, False, True))
     assert sessions is not None
     assert len(sessions) > 0 
