7b010ab3981cb92e5ef8d565f2f5feb24e0ba5d2
jeremyzyngeraneo <jzynger@aneo.fr>
2023-11-15 09:24:25 +0100

    test: add mock for test

--- tests/armonik_cli/test_unit.py
+++ tests/armonik_cli/test_unit.py
@@ -1,6 +1,9 @@
 import armonik_cli.admin as admin
 from armonik.common.filter import Filter
 import pytest
+from unittest.mock import Mock
+
+mock = Mock()
 
 def test_creating_session_filter_all():
     session_filter = admin.create_session_filter(True, False, False)
@@ -17,3 +20,18 @@ def test_creating_session_filter_cancelled():
 def test_not_creating_session_filter():
     with pytest.raises(ValueError):
         admin.create_session_filter(False, False, False)
+
+def test_creating_task_filter_all():
+    task_filter = admin.create_task_filter(["a","b"], True, False, False)
+    assert isinstance(task_filter, Filter)
+
+def test_creating_task_filter_creating():
+    task_filter = admin.create_task_filter(["a","b"], True, False, False)
+    assert isinstance(task_filter, Filter)
+
+def test_creating_task_filter_error():
+    mock = admin.create_task_filter(([1,"b"], True, False, False))
+    task_filter = admin.create_task_filter([1,"b"], True, False, False)
+    assert isinstance(task_filter, Filter)
+
+
